7546504ea6b4c3b89dafb76a06df9ee7e542ec70
Emile Rolley <emile.rolley@tuta.io>
2023-02-02 17:28:51 +0100

    nitpick: remove forgotten logging function

--- source/constantFolding.ts
+++ source/constantFolding.ts
@@ -404,7 +404,6 @@ export function constantFolding(
   engine: Engine,
   targets?: RuleName[]
 ): ParsedRules {
-  console.log("publiopti@0.1.18");
   const parsedRules: ParsedRules = engine.getParsedRules();
   let ctx: FoldingCtx = initFoldingCtx(engine, parsedRules);
 
